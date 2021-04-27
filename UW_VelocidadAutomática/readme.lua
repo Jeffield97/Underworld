b7062f3c7321550a5926faac2e9f4a90abfbd58f
pchunter14 <77764638+pchunter14@users.noreply.github.com>
2021-04-27 13:00:00 -0500

    Velocidad automática
    Presionar c para activar la velocidad constante del vehículo.

---
+++ UW_VelocidadAutomática/readme.lua
@@ -0,0 +1,20 @@
+--[[
+	Piloto automatico por ScorT^#
+	
+	-No cambies autor
+	-Si lo modificas enviame un mensaje
+	-Comparte este script
+	-Si necesitas ayuda recuerda que aqui estoy
+	(any minor changes)
+	
+	Por favor no remueva esta información, si no el script de ira a la mierda :c
+	
+	Entra a mi server de Mta: Argentina-Mexico server :D
+	
+	Facebook: Scor Mta
+	Skype: th3death89
+
+	Gracias por descargar este script.
+	
+	Atte: ScorT^#
+]]--
