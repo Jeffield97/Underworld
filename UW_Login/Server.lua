9e54a79903dd7b31689e858804bc653329f36dce
pchunter14 <77764638+pchunter14@users.noreply.github.com>
2021-05-26 09:47:35 -0500

    Update Server.lua

--- UW_Login/Server.lua
+++ UW_Login/Server.lua
@@ -99,7 +99,7 @@ function Registrar(player,User,Pass,Email,Edad,Sexo,Raza)
 				outputChatBox("#d1ebf7Contraseña: #19f730"..Pass,player,0,0,0,true)
 				logIn (player,account,Pass)
 				triggerClientEvent ( player, "CerrarRegistro",player)
-				spawnPlayer(player,1134.537109375, -1557.2515869141, 24.55,63.22)
+				spawnPlayer(player,1134.537109375, -1557.2515869141, 24.55,63.22,_,_,1)
 			end
 		else
 			exports["_infobox"]:addNotification(player,"La cuenta ya existe","error")
@@ -133,7 +133,7 @@ addEventHandler( "onResourceStart", resourceRoot,
 )
 
 function invitado(player)
-	spawnPlayer(player,1134.537109375, -1557.2515869141, 24.55,63.22)
+	spawnPlayer(player,1134.537109375, -1557.2515869141, 24.55,63.22,_,_,1)
 end
 addEvent("invitado",true)
 addEventHandler("invitado",resourceRoot,invitado)
